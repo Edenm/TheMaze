5aebae5acd476cd33db5a565d40e318b7ad94c27
Edenm <manoreden@gmail.com>
2015-11-23 15:23:16 +0200

    New classes

--- HW1_AI_Eden&Yair/src/Bina/C_AStar.java
+++ HW1_AI_Eden&Yair/src/Bina/C_AStar.java
@@ -1,3 +1,4 @@
+package Bina;
 import java.util.Comparator;
 
 public class C_AStar implements Comparator<MazeCell> {
---
+++ HW1_AI_Eden&Yair/src/Bina/MazeCell.java
@@ -0,0 +1,41 @@
+package Bina;
+
+
+public class MazeCell {
+
+	public char sign;
+	public Boolean isVisited;
+	public int iCell, jCell;
+	public MazeCell diraction;
+	public int cost;
+	public int uniformCost = 0;
+
+	public MazeCell(char charAt, int i, int j) {
+		sign = charAt;
+		iCell = i;
+		jCell = j;
+		isVisited = false;
+		if (sign == '.' || sign == 's' || sign == 'g')
+			cost = 1;
+		if (sign == ',')
+			cost = 2;
+	}
+
+	@Override
+	public String toString() {
+		return "" + sign;
+		//return "i: "+ iCell +" j: "+jCell;
+	}
+
+	public Direction whereIComeFromDiraction() {
+		if (diraction.iCell > iCell)
+			return Direction.Up;
+		if (diraction.iCell < iCell)
+			return Direction.Down;
+		if (diraction.jCell > jCell)
+			return Direction.Left;
+		else
+			return Direction.Right;
+	}
+
+}
